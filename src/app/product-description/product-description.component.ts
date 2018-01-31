0d54f90d2fb7db3f543b365e2816118eb27e0563
Andre Okamura <andre.okamura@rockstargames.com>
2018-01-30 20:29:41 -0500

    More updates

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from '../product.service';
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
